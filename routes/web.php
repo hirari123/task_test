478e02acb937fe98089c5cbc23784d1a49070369
平川祥太 <shota@hirakawashoutanoMacBook-Pro.local>
2021-04-15 11:44:12 +0900

    コンタクトルーティングの設定

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,13 @@ Route::get('/', function () {
 });
 
 Route::get('tests/test', 'TestController@index');
+
+Route::group(['prefix' =>'contact', 'middleware' => 'auth'], function(){
+    Route::get('index', 'ContactFormController@index')->name('contact.index');
+    Route::get('create', 'ContactFormController@create')->name('contact.create');
+    Route::post('store', 'ContactFormController@store')->name('contact.store');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
